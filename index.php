778ea6e39e6a1872c427e47da596cc8b2e7406ed
Josh <josh.dredge@outlook.com>
2022-10-04 23:05:52 +1000

    Document

--- index.php
+++ index.php
@@ -4,9 +4,12 @@ require_once ('EntityList.php');
 require_once ('Users.php');
 
 $allUsers = Users::where([])->select();
+
+//Successfully infers type
 $firstUser= $allUsers->first();
 echo $firstUser->birthday;
 
+//Does not infer type on the iteration
 foreach($allUsers as $user){
     echo $user->birthday;
 }
